3df022a8215386e2e5e414af9b9908e90f234549
nusivaku <nusivaku@NUSIVAKU-LAP.oradev.oraclecorp.com>
2018-10-23 15:07:40 +0530

    hiii

---
+++ src/test/java/EventHub/simpleselenium/AppTest.java
@@ -0,0 +1,20 @@
+package EventHub.simpleselenium;
+
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void sivaTest(){
+		
+		assertEquals("siva", "siva");
+		
+	}
+  
+}
